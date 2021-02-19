1d14e1e3b3afe48ca5a7fec261534b581c6710ee
MaetDol <20384262+MaetDol@users.noreply.github.com>
2021-02-19 18:28:56 +0900

    Sync to rev6 keymap
    add comma to lower layer

--- planic/keymap.c
+++ planic/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  4   |  5   |  6   |  .   |      |      |   -  |   =  |   [  |   ]  |  \   |
+ * |      |  4   |  5   |  6   |  .   |      |  ,   |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  7   |  8   |  9   |  0   |      |      |ISO # |ISO / | Home | End  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC},
-  {_______, KC_4,    KC_5,    KC_6,    KC_DOT,   XXXXXXX,  XXXXXXX,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_BSLS},
+  {_______, KC_4,    KC_5,    KC_6,    KC_DOT,   XXXXXXX,  KC_COMM,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_BSLS},
   {_______, KC_7,    KC_8,    KC_9,    KC_0,     XXXXXXX,  XXXXXXX,  KC_NUHS, KC_NUBS, KC_HOME, KC_END,  KC_DEL},
   {_______, _______, _______, _______, _______,  _______,  _______,  _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY}
 },
