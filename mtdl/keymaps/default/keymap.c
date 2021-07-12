b8ee2a779b6f42dfae6c84052fd011b0d7a1b6d7
MaetDol <rndka9@gmail.com>
2021-07-12 13:28:00 +0900

    ADD: Qwerty, Lower, Raise keymap added

--- mtdl/keymaps/default/keymap.c
+++ mtdl/keymaps/default/keymap.c
@@ -17,8 +17,9 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+  _LOWER,
+  _QWERTY,
+  _RAISE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -27,16 +28,66 @@ enum custom_keycodes {
     QMKURL
 };
 
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
-    )
+
+   /* Lower
+    * ,-----------------------------------------------------------------------------------------.
+    * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |  0   |            |
+    * |------+------+------+------+------+------+------+------+------+------+------+------------|
+    * |        |   4  |   5  |   6  |      |      |      |      |  [   |   ]  |  '   |          |
+    * |--------+------+------+------+------+------+------+------+------+------+------+----------|
+    * |             |   7  |   8  |   9  |      |      |      |      |      |   /  |            |
+    * |-------------+------+------+------+------+------+------+------+------+------+------------|
+    * |      |        |      |                                           |      |        |      |
+    * `------+--------+------+--------+-------------+-------------+------+------+--------+------'
+    */
+  [_LOWER] = LATOUY(
+		KC_GRV,  KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,      KC_8,      KC_9,      KC_0,    KC_TRNS, 
+		KC_TRNS, KC_4,    KC_5,     KC_6,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_LBRC,   KC_RBRC,   KC_QUOT, KC_TRNS,
+		KC_TRNS,          KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_SLSH, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,                       KC_TRNS,   KC_TRNS,            KC_TRNS
+  ),
+
+   /* Qwerty
+    * ,-----------------------------------------------------------------------------------------.
+    * |  ESC |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |  P   |   Bksp     |
+    * |------+------+------+------+------+------+------+------+------+------+------+------------|
+    * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  Enter   |
+    * |--------+------+------+------+------+------+------+------+------+------+------+----------|
+    * |  LShift     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  RShift    |
+    * |-------------+------+------+------+------+------+------+------+------+------+------------|
+    * | Tab  |  LAlt  | Lower|                  SPACE                    | Raise|  RALT  | GUI  |
+    * `------+--------+------+-------------------------------------------+------+--------+------'
+    */
+  [_QWERTY] = LAYOUT(
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, 
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, 
+		KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, 
+		KC_TAB,  KC_LALT, LOWER,                  KC_SPC,                     RAISE,   KC_ROPT,          KC_LGUI
+  )
+
+   /* Lower
+    * ,-----------------------------------------------------------------------------------------.
+    * |      |      |      |      |      |      |      |      |      |  Up  |      |            |
+    * |------+------+------+------+------+------+------+------+------+------+------+------------|
+    * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      | Left | Rght |      |          |
+    * |--------+------+------+------+------+------+------+------+------+------+------+----------|
+    * |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      | Down |      |            |
+    * |-------------+------+------+------+------+------+------+------+------+------+------------|
+    * |      |        |      |                                           |      |        |      |
+    * `------+--------+------+-------------------------------------------+------+--------+------'
+    */
+  [_RAISE] = LATOUY(
+		KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_UP,     KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_TRNS,   KC_LEFT,   KC_RGHT,   KC_TRNS, KC_TRNS,
+		KC_TRNS,          KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_TRNS,   KC_DOWN,   KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,                       KC_TRNS,   KC_TRNS,            KC_TRNS
+  ),
+
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
